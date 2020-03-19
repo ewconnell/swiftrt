097eb114e91c04915b25000bfaeb5f83a03c6fdf
Ed Connell <ewconnell@gmail.com>
2020-03-19 10:34:15 -0700

    added resnet stub

---
+++ Sources/SwiftRT/models/ResNetV2.swift
@@ -0,0 +1,26 @@
+//******************************************************************************
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// Original Paper:
+// "Deep Residual Learning for Image Recognition"
+// Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+// https://arxiv.org/abs/1512.03385
+// V2 paper
+// "Bag of Tricks for Image Classification with Convolutional Neural Networks"
+// Tong He, Zhi Zhang, Hang Zhang, Zhongyue Zhang, Junyuan Xie, Mu Li
+// https://arxiv.org/abs/1812.01187
+
+// A convolution and batchnorm layer
