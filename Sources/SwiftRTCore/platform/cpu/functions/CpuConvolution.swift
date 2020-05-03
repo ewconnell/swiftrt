8ff621ceab6fab19fe2e074a5b6520ae846632dc
Ed Connell <ewconnell@gmail.com>
2020-05-03 10:43:08 -0700

    snap

--- Sources/SwiftRTCore/platform/cpu/functions/CpuConvolution.swift
+++ Sources/SwiftRTCore/platform/cpu/functions/CpuConvolution.swift
@@ -140,3 +140,79 @@ extension CpuFunctions {
         fatalError("cpu convolution not implemented")
     }
 }
+
+//==============================================================================
+/// DeviceConvolution
+/// A convolution base class with a cpu default implementation. It is intended
+/// to be overriden for Cuda and other accelerated implementations.
+/// The input/output tensor type is of the form NWC, NHWC, or NDHWC
+/// Filter dimensions are defined as follows:
+/// [filter width, input channels, output channels]
+/// [filter width, filter width, input channels, output channels]
+/// [filter depth, filter width, filter width, input channels, output channels]
+public class DeviceConvolution<Shape,E,FE>
+where Shape: TensorShape,
+      E: ScalarElement, FE: ScalarElement & BinaryFloatingPoint
+{
+    public typealias Data = Tensor<Shape,E>
+    public typealias Filter = Tensor<Shape,FE>
+    public typealias Bias = TensorR1<FE>
+    
+    /// init
+    /// initializes the device function `y = convolution(x)`
+    /// - Parameter filter: the convolution filter
+    /// - Parameter bias: the filter bias vector
+    /// - Parameter activation: the activation to be applied to the result
+    /// - Parameter strides: the filter window strides
+    /// - Parameter padding: the padding surrounding `x`
+    /// - Parameter dilations: the dilations for the filter
+    /// - Parameter properties: convolution customization properties
+    /// - Parameter device: the device where the convolution will execute
+    /// - Parameter filterBiasBackpropQueueIndex: the queue to use for filter
+    /// and bias backpropagation
+    public init(activation: ActivationType,
+                strides: Shape,
+                padding: Padding,
+                dilations: Shape,
+                properties: ConvolutionProperties,
+                deviceId: Int,
+                filterBiasBackpropQueueIndex: Int) throws
+    {
+        fatalError("not implemented")
+    }
+    
+    /// infer
+    /// - Parameter y: the output tensor
+    /// - Parameter x: the input tensor
+    /// - Parameter filter: the convolution filter
+    /// - Parameter bias: the filter bias
+    //    @differentiable
+    public func infer(from x: Data, with filter: Filter, and bias: Bias)
+    throws -> Data
+    {
+        fatalError("not implemented")
+    }
+    
+    /// backPropagate
+    /// - Parameter y: the output tensor
+    /// - Parameter yDiff: the output differential
+    /// - Parameter filter: the convolution filter
+    /// - Parameter filterDiff: the filter differential
+    /// - Parameter bias: the filter bias
+    /// - Parameter biasDiff: the filter bias differential
+    /// - Parameter x: the input tensor
+    /// - Parameter x: the input tensor differential
+    public func backPropagate(
+        y: Data,
+        yDiff: Data,
+        filter: Filter,
+        filterDiff: inout Filter,
+        bias: Bias,
+        biasDiff: inout Bias,
+        x: Data,
+        xDiff: inout Data
+    ) throws
+    {
+        fatalError("not implemented")
+    }
+}
