e7fac24ba1a9defdc0715ab6cec9b8f71fc0448e
Ed Connell <ewconnell@gmail.com>
2020-06-10 14:36:47 -0700

    updated cmake

--- Package.swift
+++ Package.swift
@@ -2,7 +2,24 @@
 // The swift-tools-version declares the minimum version
 // of Swift required to build this package.
 import PackageDescription
+import Foundation
 
+// platform configuration
+//let validPlatforms = Set(arrayLiteral: "cpu", "cuda")
+//let environment = ProcessInfo.processInfo.environment
+//let platform = (environment["SWIFTRT_PLATFORM"] ?? "cpu").lowercased()
+//if !validPlatforms.contains(platform) {
+//    fatalError("valid SWIFTRT_PLATFORM types: \(validPlatforms)")
+//}
+//let buildCuda = platform == "cuda"
+
+#if os(Linux)
+let exclusions = ["*.gyb"]
+#else
+let exclusions = ["*.gyb", "platform/cuda"]
+#endif
+
+// package definition
 let package = Package(
     name: "SwiftRT",
     products: [
@@ -23,7 +40,7 @@ let package = Package(
         // core platform and base types
         .target(name: "SwiftRTCore",
                 dependencies: [.product(name: "Numerics", package: "swift-numerics")],
-                exclude: ["platform/cuda", "*.gyb"]),
+                exclude: exclusions),
 
         // tests
         .testTarget(name: "SwiftRTCoreTests", dependencies: ["SwiftRT"]),
