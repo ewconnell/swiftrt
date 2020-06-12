7e572a9d6c8c1e7582f2c153ef9c3082dd86f668
Ed Connell <ewconnell@gmail.com>
2020-06-12 10:00:21 -0700

    added test_Convolution

---
+++ Tests/SwiftRTLayerTests/test_Convolution.swift
@@ -0,0 +1,36 @@
+//******************************************************************************
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+import XCTest
+import Foundation
+import SwiftRT
+
+class test_Convolution: XCTestCase {
+    static var allTests = [
+        ("test_Tensor2", test_Tensor2),
+        ("test_Image", test_Image),
+    ]
+
+    //--------------------------------------------------------------------------
+    func test_Tensor2() {
+//        let c = Convolution2(filterShape: [3, 3], filterInitializer: glorotUniform())
+//        let i = Convolution<Shape2,Int8,Float>(filterShape: [3, 3],
+//                                               filterInitializer: glorotUniform())
+    }
+
+    //--------------------------------------------------------------------------
+    func test_Image() {
+    }
+}
