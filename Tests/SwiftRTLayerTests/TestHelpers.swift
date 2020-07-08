cd7c5c02fbf89284f54745b00ce5062fc6a8bd96
Ed Connell <ewconnell@gmail.com>
2020-07-08 15:56:15 -0700

    handled linux vs macOS diff for #file

--- Tests/SwiftRTLayerTests/TestHelpers.swift
+++ Tests/SwiftRTLayerTests/TestHelpers.swift
@@ -1,7 +1,36 @@
 import XCTest
 import SwiftRT
 
-// TODO: Convert message to an @autoclosure.
+#if os(Linux)
+func assertEqual<T: FloatingPoint>(
+    _ x: [T], _ y: [T], accuracy: T, _ message: String = "",
+    file: StaticString = #file, line: UInt = #line)
+{
+    for (x, y) in zip(x, y) {
+        if x.isNaN || y.isNaN {
+            XCTAssertTrue(x.isNaN && y.isNaN,
+                          "\(x) is not equal to \(y) - \(message)",
+                          file: file, line: line)
+        } else {
+            XCTAssertEqual(x, y, accuracy: accuracy, message,
+                           file: file, line: line)
+        }
+    }
+}
+
+func assertEqual<Shape, T: FloatingPoint>(
+    _ x: Tensor<Shape, T>,
+    _ y: Tensor<Shape, T>,
+    accuracy: T,
+    _ message: String = "",
+    file: StaticString = #file,
+    line: UInt = #line
+) where T.Value == T {
+    assertEqual(x.flatArray, y.flatArray, accuracy: accuracy,
+                message, file: file, line: line)
+}
+
+#else
 
 func assertEqual<T: FloatingPoint>(
     _ x: [T], _ y: [T], accuracy: T, _ message: String = "",
@@ -30,3 +59,4 @@ func assertEqual<Shape, T: FloatingPoint>(
     assertEqual(x.flatArray, y.flatArray, accuracy: accuracy,
                 message, file: file, line: line)
 }
+#endif
