e8f7024abbd3f593d61c4238e006fb59b2491312
Ed Connell <ewconnell@gmail.com>
2020-07-15 15:18:56 -0700

    added stub for MatrixLayout

--- Sources/SwiftRTCore/platform/cuda/cublaslt/MatrixLayout.swift
+++ Sources/SwiftRTCore/platform/cuda/cublaslt/MatrixLayout.swift
@@ -22,6 +22,7 @@ public final class MatrixLayout {
     // properties
     public let desc: cublasLtMatrixLayout_t
 
+    //--------------------------------------------------------------------------
     // initializers
     @inlinable public init(
         type: cudaDataType,
@@ -38,5 +39,55 @@ public final class MatrixLayout {
     @inlinable deinit {
         cudaCheck(cublasLtMatrixLayoutDestroy(desc))
     }
+
+    //--------------------------------------------------------------------------
+    /// getAttribute
+    @inlinable public func getAttribute<T>(
+        _ attr: cublasLtMatrixLayoutAttribute_t, 
+        _ value: inout T
+    ) {
+        var written = 0
+        cudaCheck(cublasLtMatrixLayoutGetAttribute(
+            desc, attr, &value, MemoryLayout.size(ofValue: value), &written))
+    }
+
+    /// setAttribute
+    @inlinable public func setAttribute<T>(
+        _ attr: cublasLtMatrixLayoutAttribute_t,
+         _ value: inout T
+    ) {
+        cudaCheck(cublasLtMatrixLayoutSetAttribute(
+            desc, attr, &value, MemoryLayout.size(ofValue: value)))
+    }
+
+    //--------------------------------------------------------------------------
+    /// Specifies the data precision type
+    @inlinable public var type: ScalarType {
+        get {
+            var value = CUDA_R_32F
+            getAttribute(CUBLASLT_MATRIX_LAYOUT_TYPE, &value)
+            return ScalarType(value)
+        }
+        set {
+            var value = newValue.cuda
+            setAttribute(CUBLASLT_MATRIX_LAYOUT_TYPE, &value)
+        }
+    }
+
+    // //--------------------------------------------------------------------------
+    // /// Specifies the data precision type
+    // @inlinable public var order: ScalarType {
+    //     get {
+    //         var value = CUDA_R_32F
+    //         getAttribute(CUBLASLT_MATRIX_LAYOUT_ORDER, &value)
+    //         return ScalarType(value)
+    //     }
+    //     set {
+    //         var value = newValue.cuda
+    //         setAttribute(CUBLASLT_MATRIX_LAYOUT_ORDER, &value)
+    //     }
+    // }
+
+
 }
 
