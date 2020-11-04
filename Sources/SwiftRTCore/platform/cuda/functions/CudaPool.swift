8c6ea5a7a1382f8c5dc659e18b31126d473c7e6c
Ed Connell <ewconnell@gmail.com>
2020-11-04 14:58:17 -0800

    snap

--- Sources/SwiftRTCore/platform/cuda/functions/CudaPool.swift
+++ Sources/SwiftRTCore/platform/cuda/functions/CudaPool.swift
@@ -44,8 +44,8 @@ extension CudaQueue {
 }
 
 //----------------------------------------------------------------------------
-public struct PoolingConfiguration {
-  // let poolingDesc: PoolingDescriptor
+public class PoolingConfiguration {
+  let poolingDesc: PoolingDescriptor
   // let xDesc: TensorDescriptor
   // let outDesc: TensorDescriptor
 
@@ -59,13 +59,17 @@ public struct PoolingConfiguration {
   ) {
     // create descriptor
     // let poolingRank = inData.extent.count - 2
+    let padding = S.zero
     // let padding = expand(array: props.pad, to: poolingRank)
     // let windowSize = expand(array: props.windowSize, to: poolingRank)
     // let stride = expand(array: props.stride, to: poolingRank)
 
-    // poolingDescriptor = PoolingDescriptor(
-    //   mode: props.mode, nan: props.nan, rank: poolingRank,
-    //   window: windowSize, padding: padding, stride: stride)
+    poolingDescriptor = PoolingDescriptor(
+      mode: mode,
+      nan: .propagate,
+      window: size,
+      padding: padding,
+      strides: strides)
 
     // // create input tensor descriptor
     // inTensor = try inData.createTensorDescriptor()
@@ -82,4 +86,39 @@ public struct PoolingConfiguration {
     // outTensor = try outData.createTensorDescriptor()
   }
 
+}
+
+//==============================================================================
+// PoolingDescriptor
+public struct PoolingDescriptor<Shape: TensorShape> {	
+	// properties
+	let desc: cudnnPoolingDescriptor_t
+
+	// initializers
+	public init(
+    mode: PoolingMode,
+    nan: NanPropagation,
+    window: Shape,
+	  padding: Shape,
+    stride: Shape
+  ) {
+		// create the descriptor
+		var temp: cudnnPoolingDescriptor_t?
+		try cudaCheck(status: cudnnCreatePoolingDescriptor(&temp))
+		desc = temp!
+		
+		// initialize
+		try cudaCheck(status: cudnnSetPoolingNdDescriptor(
+			desc,
+      mode.cudnn,
+      nan.cudnn,
+			CInt(Shape.rank),
+			window.map { CInt($0) },
+			padding.map { CInt($0) },
+			stride.map { CInt($0) }))
+	}
+
+	deinit {
+		cudaCheck(cudnnDestroyLRNDescriptor(desc))
+	}
 }
